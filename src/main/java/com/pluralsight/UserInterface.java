f3e74016d1e8ffeab56643ebf4178aeaadf71bab
KatherineColon <katherinecolon@onmail.com>
2024-05-10 15:56:17 -0400

    initial commit

---
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class UserInterface {
+    Dealership dealership = new Dealership();
+    public static String display(){
+    return "hi"; //fix later
+    }
+}
---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+public class Vehicle {
+
+
+    //variables
+    int vin;
+    int year;
+    String make;
+    String model;
+    String vehicleType;
+    String color;
+    int odometer;
+    double price;
+
+    //constructor
+    public Vehicle (int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price){
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+
+
+}
+}
+
